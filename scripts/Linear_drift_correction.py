e5c844e7b0179e4a431baab5821d86661e2e8020
Cano-Muniz, Santiago <33370667+phisanti@users.noreply.github.com>
2023-03-01 20:14:13 +0000

    Create Linear_drift_correction.py

---
+++ scripts/Linear_drift_correction.py
@@ -0,0 +1,21 @@
+from ij.gui import NewImage
+from ij import IJ
+
+imp = IJ.getImage()
+def drift_correction(imp):
+	
+    """Drift correction for images stack where the object displace linearly in one direction.
+    :param imp: ImagePlus object
+    """
+
+	stack = imp.getStack()
+	n =stack.size()
+	for i in range(n):
+		x = -i * 0.01
+		y = -i * 0.01
+		imp.setSlice(i + 1)
+		IJ.run(imp, "Translate...", "x=" + str(x) +" y=" + str(y) + " interpolation=None slice");
+
+	imp.show()
+
+	return True
